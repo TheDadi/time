3532d62af0abba72e88d0f346140ea633323f404
ddifazio <ddifazio@10.33.29.24>
2014-06-13 11:56:33 +0200

    fix unique Commit from JBoss Tools

--- src/main/java/ch/lepeit/stundenabrechnung/model/Task.java
+++ src/main/java/ch/lepeit/stundenabrechnung/model/Task.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -35,6 +37,7 @@ public class Task implements Serializable {
   	private Benutzer benutzer;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int task_id; 
     
     private String name;
